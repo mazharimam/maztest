9501d9cf8d36e6a4a326381dfd7ac5911198efb1
mazharimam <mazhar.tech@live.com>
2017-04-20 21:45:17 +0530

    Add files via upload
    Test

---
+++ FirstProg.java
@@ -0,0 +1,15 @@
+package com.test.java;
+
+public class FirstProg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Mazhar");
+		System.out.println("My First Prog");
+
+	}
+
+}
